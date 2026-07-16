31044060ae9f0343268325144385682805f22240
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-07-16 10:40:44 +0700

    membuat void input

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -4,3 +4,32 @@ using namespace std;
 int arr[20]; // membuat array dengan panjang data 20
 int n;       // membuat variabel n untuk input panjang array
 
+void input() // membuat prosedur input
+{
+    while (true)
+    {
+        cout << "Masukkan Jumlah Data Pada Array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nPanjang Data Maksimal 20" << endl;
+        }
+    }
+
+    cout << endl;
+    cout << "======================" << endl;
+    cout << "Masukkan Element Array" << endl;
+    cout << "======================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Masukkan data ke " << i + 1 << " : ";
+        cin >> arr[i];
+    }
+}
+
