97290c42a29dad5a8fd54186de7f3423ff243a39
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-07-16 10:41:25 +0700

    membuat insertion sort

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -33,3 +33,28 @@ void input() // membuat prosedur input
     }
 }
 
+void insertionSort() // membuat prosedur insertion sort
+{
+    int temp, i, j;
+
+    for (i = 1; i <= n - 1; i++)
+    {
+        temp = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = temp;
+
+        cout << "\nPass " << i << " : ";
+        for (int k = 0; k < n; k++)
+        {
+            cout << arr[k] << " ";
+        }
+    }
+}
+
